b1eca789fd21704cd24c2b830cd9375dfddce63c
Dhruv Joshi LVP <dhruv.joshi.1989@gmail.com>
2016-01-25 12:54:44 +0530

    stable before testing getdata(vid)

--- wcam_test.m
+++ wcam_test.m
@@ -1,6 +1,15 @@
+%% close previous shit
+try
+   stop(w1);  stop(w2);
+   clear('w1'); clear('w2');
+   close(v);
+end
+
 %% global variables - these aren't really being used now
 threshold_dark = 0.8;
 threshold_bright = 0.87;
+
+h = rapdi_gui;
 
 % setup webcams using the object that was made using the image acquisition
 % toolbox. We will set trigger to manual so that we can call frames as they
@@ -14,13 +23,22 @@ w2 = webcam2;
 triggerconfig(w2, 'manual');
 start(w2);
 
+v = VideoWriter('video.avi');
+open(v);
+
 while(1)
    % loop to collect images, process them and then display them 
-   im1 = process_pupil(getsnapshot(w1), 1, 100,threshold_dark, threshold_bright);
-   im2 = process_pupil(getsnapshot(w2), 1, 100,threshold_dark, threshold_bright);
+   im1 = getsnapshot(w1);
+   im2 = getsnapshot(w2);
+   
+   imm = horzcat(im1, im2);
+   writeVideo(v, imm);
+   
+   im1 = process_pupil(im1, 1, 100,threshold_dark, threshold_bright);
+   im2 = process_pupil(im2, 1, 100,threshold_dark, threshold_bright);
       
    imshow([im1, im2], 'InitialMagnification', 67);
 end
 
 % close the connection to the object
-stop('w1');
+stop('w1'); stop('w2'); close(v);
