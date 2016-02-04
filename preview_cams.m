639384f00e6ad12bf976cac1e2a1d906b9b96018
Dhruv Joshi LVP <dhruv.joshi.1989@gmail.com>
2016-02-04 11:49:24 +0530

    able to write hte videos to different files and pass as argument to UpdatePreviewFunc

--- preview_cams.m
+++ preview_cams.m
@@ -3,8 +3,34 @@
 % Also save the live video stream at the end
 % If this is possible, then the whole thing should happen this way
 global record;
+global arduino;
 record = 0;     % false
 
+%% First get inputs from the examiner
+% h = rapdi_gui;
+% uiwait(h); 
+
+%% Create the video object
+video_filename = strcat(mr_no, '_left.avi');
+v_left = VideoWriter(video_filename);
+v_left.FrameRate = 30;
+v_left.Quality = 100;
+
+video_filename = strcat(mr_no, '_right.avi');
+v_right = VideoWriter(video_filename);
+v_right.FrameRate = 30;
+v_right.Quality = 100;
+
+%% Establish connection to the arduino
+%{
+ports = instrhwinfo('serial');
+ports_list = ports.AvailableSerialPorts;
+arduino_port = ports_list(2);    % TODO: establish comm to prove it is the arduino
+arduino = serial(arduino_port{1});  % establish comm.
+set(arduino,'BaudRate',9600);
+fopen(arduino);
+%}
+%% Establish connection to the cameras
 if (~exist('w1'))
     w1 = webcam1;
     triggerconfig(w1, 'manual');
@@ -28,11 +54,13 @@ subplot(1,2,1); h1 = subimage(hImage1);     % h1 and h2 are the handles to the s
 subplot(1,2,2); h2 = subimage(hImage2);
 
 gui_handle = guidata(f);
-setappdata(h2, 'UpdatePreviewWindowFcn', @process_videos_func);
+
+setappdata(h1, 'UpdatePreviewWindowFcn', @(obj, evt, h1)process_videos_func(obj, evt, h1, v_left));
+setappdata(h2, 'UpdatePreviewWindowFcn', @(obj, evt, h2)process_videos_func(obj, evt, h2, v_right));
 
 preview(w1, h2); preview(w2, h1);           % display the images as subplots
 % start_button = uicontrol(f,'Style','pushbutton','String','START', 'Position',[50 20 60 40], 'Callback', @(hObject, callbackdata)setappdata(h1, 'UpdatePreviewWindowFcn', @process_videos_func));  % using anonymous function like a boss
 % stop_button = uicontrol(f,'Style','pushbutton','String','STOP', 'Position',[200 20 60 40], 'Callback', @(hObject, callbackdata)setappdata(h1, 'UpdatePreviewWindowFcn', []));
 
 start_button = uicontrol(f,'Style','pushbutton','String','START', 'Position',[50 20 60 40], 'Callback', {@do_record, '1'});
-stop_button = uicontrol(f,'Style','pushbutton','String','STOP', 'Position',[200 20 60 40], 'Callback', {@do_record, '0'});
+stop_button = uicontrol(f,'Style','pushbutton','String','STOP', 'Position',[450 20 60 40], 'Callback', {@do_record, '0'});
--- process_videos_func.m
+++ process_videos_func.m
@@ -1,9 +1,10 @@
-function process_videos_func(obj, event, himage)
+function process_videos_func(obj, event, himage, videoObj)
     % this is called each time the camera makes a new frame available.
     % the input arguments are passed automatically. You can add others if you need 
     % them, but they also have to be included where this function is created (above)
     % do all your real-time processing here in this function
     global record;
+    global arduino;
     
     threshold_dark = 0.8;
     threshold_bright = 0.87;
@@ -13,14 +14,19 @@ function process_videos_func(obj, event, himage)
 
     % you get the image in this function from:
     im = event.Data;
-    im = process_pupil(im, 1, 180,threshold_dark, threshold_bright);
+    % im_processed = process_pupil(im, 1, 180,threshold_dark, threshold_bright);
     
     if (record == 1)
         % send ON command to arduino
         % create video object and start recording
+        % fprintf(arduino, 's');
+        open(videoObj);
+        writeVideo(videoObj, im);      %% THIS IS GOING TO GIVE STRANGE OUTPUT
     else
         % send OFF command to arduino
         % terminate videoObj
+        close(videoObj);
+        % calculate FPS using timestamps and save the video
     end
 
     % you actively need to display the image in this function
